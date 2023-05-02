51c23562497a3d89bf36915f11107a884d032e88
Geonha Lee <ipjaworld@gmail.com>
2023-05-03 00:08:48 +0900

    메인 레이아웃 헤더

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -2,18 +2,20 @@
   <div>
     <header>
       <nav>
-        <ul>
-          <li><nuxt-link to="/">메인</nuxt-link></li>
-          <li><nuxt-link to="/character">캐릭터</nuxt-link></li>
-          <li><nuxt-link to="/news">뉴스</nuxt-link></li>
-          <li><nuxt-link to="/simulator">시뮬레이터</nuxt-link></li>
-          <li><nuxt-link to="/community">커뮤니티</nuxt-link></li>
-          <li><nuxt-link to="/ranking">랭킹(신기능)</nuxt-link></li>
+        <ul class="flex">
+          <li class="mr-4"><nuxt-link to="/">메인</nuxt-link></li>
+          <li class="mr-4"><nuxt-link to="/character">캐릭터</nuxt-link></li>
+          <li class="mr-4"><nuxt-link to="/news">뉴스</nuxt-link></li>
+          <li class="mr-4"><nuxt-link to="/simulator">시뮬레이터</nuxt-link></li>
+          <li class="mr-4"><nuxt-link to="/community">커뮤니티</nuxt-link></li>
+          <li class="mr-4"><nuxt-link to="/ranking">랭킹(신기능)</nuxt-link></li>
         </ul>
       </nav>
       <nav>
-        <nuxt-link to="/login">로그인</nuxt-link>
-        <nuxt-link to="/join">회원가입</nuxt-link>
+        <ul class="flex">
+          <li class="mr-4"><nuxt-link to="/login">로그인</nuxt-link></li>
+          <li><nuxt-link to="/join">회원가입</nuxt-link></li>
+        </ul>
       </nav>
     </header>
     <main>
@@ -38,15 +40,22 @@ main {
   min-height: 600px;
 }
 header {
+  background-color: blueviolet;
   margin: 0 auto;
-  width: 1200px;
+  height: 80px;
+  width: 100%;
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
 }
 header nav:first-child {
   display: flex;
 }
 header nav:last-child {
   display: flex;
+}
+main {
+  min-height: 900px;
 }
 </style>
--- pinia/auth.ts
+++ pinia/auth.ts
@@ -8,7 +8,7 @@ export const useAuthStore = defineStore('auth', {
     isAuthenticated: (state) => state.authenticated,
   },
   actions: {
-    setAuthenticated(authenticated) {
+    setAuthenticated(authenticated: boolean) {
       this.authenticated = authenticated
     },
     // 필요한 인증 관련 액션을 여기에 추가하십시오.
